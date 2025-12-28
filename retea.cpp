b3dbf1b21c2dfc1b01e1f49fa3e6ed17c140fb2a
Vitali <120036293+Vitali17@users.noreply.github.com>
2025-12-28 22:08:59 +0200

    Update retea.cpp

--- retea.cpp
+++ retea.cpp
@@ -96,50 +96,97 @@ int main() {
 
     srand(time(NULL));
 
-    cout<<"==============Simulator de retea==============";
+    cout<<"=============================================Simulator de retea=============================================";
     cout<<endl;
-    int x;
-    cout<<"Introduceti numarul de noduri:";
-    cin>>x;
-    cout<<endl;
-    Retea *retea = new Retea(x);
+    cout<<"Acest program simuleaza o retea de calculatoare, permitand crearea de noduri stergerea lor si transmiterea de date"<<endl;
+    cout<<"Tastati 1 pentru a incepe sau 0 pentru a iesi:";
+    int z;
+    cin>>z;
+    for (int i=0;i<50;i++)
+        cout<<endl;
+    switch (z) {
+        case 1:{
+            int x;
+            cout<<"Introduceti numarul de noduri:";
+            cin>>x;
+            cout<<endl;
+            Retea *retea = new Retea(x);
 
-    while(true) {
+            while(true) {
+                for (int i=0;i<50;i++)
+                    cout<<endl;
+                cout<<"Introducrti 1 pentru a crea legaturi"<<endl;
+                cout<<"Introduceti 2 pentru a afisa legaturile"<<endl;
+                cout<<"Introducrti 3 pentru a trimite pachet"<<endl;
+                cout<<"Introducrti 4 pentru a iesi"<<endl;
 
-        cout<<"Introducrti 1 pentru a crea legaturi"<<endl;
-        cout<<"Introduceti 2 pentru a afisa legaturile"<<endl;
-        cout<<"Introducrti 3 pentru a trimite pachet"<<endl;
+                int y;
 
-        int y;
+                cin>>y;
 
-        cin>>y;
+                switch (y) {
+                    case 1:
+                        for (int i = 0; i < 50; i++)
+                            cout << endl;
 
-        switch (y) {
-            case 1:
-                int x1,x2;
-                cout<<"Pentru a crea un nod introduceti doua adrese care sa fie strict mai mici de "<<x<<" si mai mari sau egal cu 0";
-                cout<<endl;
-                cout<<"Pentru a iesi tastati -1 -1"<<endl;
-                cin>>x1>>x2;
-                while (x1 >= 0 && x2>=0) {
-                    retea->adaugaLegatura(x1,x2);
-                    A:
-                    cin>>x1>>x2;
-                    if (x1>=x || x2>=x) {
-                        cout<<"Eroare: Valoarea introdusa nu poate fi nod introduceti alta valoare";
-                        cout<<endl;
-                        goto A;
+                        int x1, x2;
+
+                        cout << "Pentru a crea un nod introduceti doua adrese strict mai mici de "
+                             << x << " si mai mari sau egale cu 0\n";
+                        cout << "Pentru a iesi tastati -1 -1\n";
+
+                        while (true) {
+                            cin >> x1 >> x2;
+
+                            if (x1 == -1 && x2 == -1)
+                                break;
+
+                            if (x1 < 0 || x2 < 0 || x1 >= x || x2 >= x || x1 == x2) {
+                                cout << "Eroare: Valoarea introdusa nu poate fi nod. Incercati din nou.\n";
+                                continue;
+                            }
+
+                            retea->adaugaLegatura(x1, x2);
                     }
+                        break;
+
+                    case 2:
+                        for (int i=0;i<50;i++)
+                            cout<<endl;
+                        int ex;
+                        cout<<"Legaturile create sunt:"<<endl;
+                        retea -> afiseaza();
+                        cout<<endl;
+                        cout<<"Pentru a iesi tastati orice valoare";
+                        cin>>ex;
+                        break;
+                    case 3:
+                        for (int i=0;i<50;i++)
+                            cout<<endl;
+                        cout<<"Pentru a trimite un pachet introduceti sursa si destinatorul\n";
+                        A:
+                        cout<<"Sursa:";
+                        int sursa, destinator;
+                        cin>>sursa;
+                        cout<<"Introduceti destinatorul:";
+                        cin>>destinator;
+                        cout<<endl;
+                        retea -> TrimitePachet(sursa,destinator);
+                        cout<<endl;
+                        cout<<"Pentru a trimite un alt pachet introduce 1 in caz contrar reveniti la programul principal:";
+                        cin>>ex;
+                        if (ex==1) goto A;
+                        else break;
+
+                    case 4:
+                        return 2;
                 }
-                break;
-            case 2:
-                retea -> afiseaza();
-                break;
-        }
+                }
+            }
+        case 0:
+            return 1;
 
     }
-
-
 
     // Retea r(6);
     //
